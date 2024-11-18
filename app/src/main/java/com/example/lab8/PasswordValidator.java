c85f667a661aba828b90bc58cde0409ff313a24e
Miranda Deng <mdeng027@uottawa.ca>
2024-11-18 12:21:31 -0500

    Initial commit

---
+++ app/src/main/java/com/example/lab8/PasswordValidator.java
@@ -0,0 +1,23 @@
+package com.example.lab8;
+
+public class PasswordValidator {
+
+    public static boolean isValidPassword(String password) {
+        if (password == null) return false;
+
+        if (password.length() < 10) return false;
+
+        boolean hasUpperCase = false;
+        boolean hasLowerCase = false;
+        boolean hasNumber = false;
+
+        for (char c : password.toCharArray()) {
+            if (Character.isUpperCase(c)) hasUpperCase = true;
+            if (Character.isLowerCase(c)) hasLowerCase = true;
+            if (Character.isDigit(c)) hasNumber = true;
+        }
+        // If both conditions are met, the password is valid
+        if (hasUpperCase && hasLowerCase && hasNumber) return true;
+        return false;
+    }
+}
